65b4475ed534b287e17538ac4bd74aec8ff10632
chocolateboysandip@gmail.com <chocolateboysandip@gmail.com@a3c056ee-30e3-bb7b-1c11-d40ccffc169f>
2014-09-21 22:03:46 +0000

    Form validation success edition.

---
+++ trunk/springwebtest/src/com/springwebtest/controllers/UserMgmtController.java
@@ -0,0 +1,36 @@
+package com.springwebtest.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.*;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.springwebtest.beans.testbeans.User;
+
+@Component
+@RequestMapping("/userMgmt")
+public class UserMgmtController {
+	
+	@RequestMapping(value="/addUser", method=RequestMethod.GET)
+	   public String addNewUser(Model model){
+		   model.addAttribute(new User());
+		   System.out.println("=======>Got request...!!!!");
+		   return "views/jsps/userRegistration.jsp";
+	   }
+	
+	   @RequestMapping(value="/addUser", method=RequestMethod.POST)
+	   public String addUserForm(@Valid User user, BindingResult bindingResult, @RequestParam(value="image",required=false) MultipartFile image){
+		   System.out.println("=======>In post..");
+	       if(bindingResult.hasErrors()){
+              System.out.println("=======>In post error..");
+	          return "views/jsps/userRegistration.jsp";
+	       }
+	       System.out.println("=======>In post success..");
+	       return "redirect:/hello/";
+	   }
+}
